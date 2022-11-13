36ae210996bd38f7d25465d48d4b79ee6e336ce2
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-11-12 21:16:56 -0300

    Realizado a configuração do typescript, script de automatização e refatoração na arquitetura do projeto

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,11 @@
 console.log("Start JS");
+
 import { Negociacao } from "./models/negociacao.js";
+
 const negociacao = new Negociacao(new Date(), 10, 100);
 console.log(negociacao);
 console.log(negociacao.data);
 console.log(negociacao.volume);
-negociacao.setQuantidade(10);
+negociacao.setQuantidade(10)
 console.log(negociacao.quantidade);
 console.log(negociacao);
--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,22 +1,35 @@
 export class Negociacao {
+    // Criando variaveis com # em classe no JS, definimos uma classe privada: #data
+
+    //No typescript seguimos da seguinte maneira ao definir a variável privada da classe. quando for utilizar na classe para geters, basta colocar _nomeDaVar
+    private _data;
+    private _quantidade;
+    private _valor;
+
     constructor(data, quantidade, valor) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
+
     get data() {
         return this._data;
     }
+
     get quantidade() {
         return this._quantidade;
     }
+
     get valor() {
         return this._valor;
     }
+
     get volume() {
         return this._quantidade * this._valor;
     }
+
     setQuantidade(quantidade) {
         this._quantidade = quantidade;
     }
+   
 }
